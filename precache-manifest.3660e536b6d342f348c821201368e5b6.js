f21b45d1437babdbfb05838ebf60abe91d53e0ad
Dat Dang <46439840+datdang1995@users.noreply.github.com>
2020-03-27 15:26:30 +0700

    Updates

---
+++ precache-manifest.3660e536b6d342f348c821201368e5b6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de2f80f9b9a4079ed4af2c7b7d3cc4a3",
+    "url": "/games-online/index.html"
+  },
+  {
+    "revision": "1360826af95839d65c75",
+    "url": "/games-online/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "df41022a5ff9427286d6",
+    "url": "/games-online/static/css/main.9ba037eb.chunk.css"
+  },
+  {
+    "revision": "1360826af95839d65c75",
+    "url": "/games-online/static/js/2.1df4b38f.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/games-online/static/js/2.1df4b38f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df41022a5ff9427286d6",
+    "url": "/games-online/static/js/main.3f415a47.chunk.js"
+  },
+  {
+    "revision": "b3c2953b2ff2eb173723",
+    "url": "/games-online/static/js/runtime-main.9071c0ff.js"
+  },
+  {
+    "revision": "64a069f7e0f1c5948c5f56f580dfdfbf",
+    "url": "/games-online/static/media/404-bg.64a069f7.jpg"
+  },
+  {
+    "revision": "0f219abaa24f167049332c3cec46364a",
+    "url": "/games-online/static/media/cs-go-icon.0f219aba.png"
+  },
+  {
+    "revision": "2b8fbb44fcee58bf4374879812a97994",
+    "url": "/games-online/static/media/cs-go.2b8fbb44.jpg"
+  },
+  {
+    "revision": "580c366db2f052402ef2a72dc628804d",
+    "url": "/games-online/static/media/dota-2-icon.580c366d.png"
+  },
+  {
+    "revision": "2d33d761c87c35e261b1f5c79f1c5b5e",
+    "url": "/games-online/static/media/dota-2.2d33d761.jpg"
+  },
+  {
+    "revision": "77ab3cbb68058d3a2ae459365aa8dc11",
+    "url": "/games-online/static/media/hearthstone-icon.77ab3cbb.png"
+  },
+  {
+    "revision": "7f5728082633be34189ce8549de7a36e",
+    "url": "/games-online/static/media/hearthstone.7f572808.jpg"
+  },
+  {
+    "revision": "33ee95c4a283a446eac8e899c7302364",
+    "url": "/games-online/static/media/overwatch-icon.33ee95c4.png"
+  },
+  {
+    "revision": "679e1f185406d3a23f429adc862f9988",
+    "url": "/games-online/static/media/overwatch.679e1f18.png"
+  }
+]);
